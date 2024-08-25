d36298a0c8ba71b0d32d2a7ea45d41d78d4ac320
tomoaoihana <tamu.kusanishi@gmail.com>
2024-08-26 06:23:11 +0900

    MarkDownの調整など

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import openAi from "./lib/openAi";
+import Markdown from "react-markdown";
 
 const prompt = `
 あなたは20年以上キャリアがあるフルスタックエンジニアです。
@@ -15,16 +16,23 @@ const prompt = `
 function App() {
   const [content, setContent] = useState("");
   const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const review = async () => {
+    //isLoadingをtrueにする
+    setIsLoading(true);
+
     //messagesの作成
     //OpenAIのAPIに渡すmessagesの形式に合わせる
     const messages = [{ role: "user", content: prompt + content }];
+
     //APIの呼び出し
     //準備したmessagesを引数にしてopenAi.completionを呼び出す
     const result = await openAi.completion(messages);
-    //結果をsetResultで保存
+
     setResult(result);
+    //isLoadingをfalseにする
+    setIsLoading(false);
   };
   return (
     <div className="min-h-screen bg-blue-100 flex flex-col items-center justify-center">
@@ -40,14 +48,21 @@ function App() {
             />
           </div>
           <button
+            disabled={isLoading}
             onClick={review}
             className="bg-indigo-600 hover:bg-indigo-700 text-white py-2 px-4 rounded-md focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
           >
-            レビューする
+            {isLoading ? "レビュー中..." : "レビューする"}
           </button>
         </div>
         <div className="flex flex-col w-1/2 h-full items-center justify-center">
-          <div className="p-4 overflow-y-auto w-full">{result}</div>
+          <div className="p-4 overflow-y-auto w-full">
+            {isLoading ? (
+              "レビュー中"
+            ) : (
+              <Markdown className={"markdown"}>{result}</Markdown>
+            )}
+          </div>
         </div>
       </main>
     </div>
